cfc4d58efde7c9e92828ec5aa1764f01842c1de1
ElOso064 <josuelist277@gmail.com>
2022-09-14 12:55:29 -0500

    finished assignment 02.8

--- src/main/kotlin/week02/StringManipulator028.kt
+++ src/main/kotlin/week02/StringManipulator028.kt
@@ -3,11 +3,13 @@ package week02
 fun main(){
     println("Please enter the name of your favorite city >> ")
     val favCity = readLine()!!
-    val favCityLength = favCity.length
-    println("The length of letter in the city are " favCityLength)
-    val cityUpperCase = favCity.uppercase()
-    println("City in all caps: " cityUpperCase)
-    val cityLowerCase = favCity.lowercase()
-    println("City in all lower case " cityLowerCase)
+     if (favCity != null){
+         println("The number of characters in the city name are: " + favCity.length)
+         println("The name of the city in upper case: " + favCity.uppercase())
+         println("The name in city in all lower case: " + favCity.lowercase())
+         println("The first character in the name of the city is: " +favCity.first())
+         // Josue Barajas - Assignment String Manipulator 02.8 - Sept 14th 2022
+     }
+
 
 }
