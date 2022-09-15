814da0dcf9d18d7472debf8933cfbd8227b95004
ElOso064 <josuelist277@gmail.com>
2022-09-15 10:36:43 -0500

    Finished assignment 04.5

---
+++ src/main/kotlin/week04/RandomNumberGuessingGame045.kt
@@ -0,0 +1,23 @@
+package week04
+// Josue Barajas - September 15th - 04.5 Assignment
+fun main() {
+
+ val myNumber = (0  .. 10).random()
+
+    println("Guess a number 1-10:")
+    val guess = Integer.valueOf(readLine())
+
+    println("My number is: " +myNumber)
+    println("Your guess: " +guess)
+        if (guess < myNumber){
+            println("Your guess is too low, go up a little!")
+        }
+
+        if (guess > myNumber){
+            println("Your guess is too high, go down a little!")
+        }
+
+        if (guess == myNumber){
+            println("Bingo! You guessed my number!!")
+        }
+}
