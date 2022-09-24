84804d5d97d9f759c0726542588603b434d9ed0b
ElOso064 <josuelist277@gmail.com>
2022-09-23 19:38:49 -0500

    Completed Assignment 04.6

--- src/main/kotlin/week04/ESPGame046.kt
+++ src/main/kotlin/week04/ESPGame046.kt
@@ -1,11 +1,19 @@
 package week04
 // Josue Barajas - 04.6 Assignment - 09/21/2022
 fun main(){
-    val color1 = "Red"
-    val color2 = "Green"
-    val color3 = "Blue"
-    val color4 = "Orange"
-    val color5 = "Yellow"
-
-    
+    val randomColor = arrayOf<String> ("red","green","blue","orange","yellow")
+    var correctCount = 0
+    println("The random colors are green, blue, red, orange and yellow.")
+    println("You will have 10 guesses! I will display the ones that are correct")
+        for (i in 1 .. 10) {
+           val color =  randomColor.random()
+            println("Please select a color from the colors provided above >> ")
+            val userColor = readLine()
+            if (userColor == color){
+                println("Bingo! You guessed it right!")
+                correctCount ++
+            }  else {
+                println("Try again, the correct color was $color")
+            }
+        }
 }
