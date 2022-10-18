0a492b7171c009dc4dadb1e5ff10a2d19bcd543d
ElOso064 <josuelist277@gmail.com>
2022-10-18 10:13:32 -0500

    Completed Assignment 04.6

---
+++ src/main/kotlin/week05/PrimeTime051.kt
@@ -0,0 +1,25 @@
+package week05
+// Josue Barajas - 05.1 Assignment - October 18th, 2022
+    fun main() {
+    println(isPrime(13))
+    }
+
+    fun isNumberDivisible(number: Int, divisor: Int): Boolean {
+
+       var results: Boolean = false
+        if(number%divisor == 0){
+        results = true
+        }
+        return results
+    }
+
+    fun isPrime(number: Int): Boolean{
+        var results: Boolean = false
+        if(number <= 0)
+            return false
+        for(i in 2 until number)
+        if(isNumberDivisible(number,i)){
+            return false
+        }
+        return true
+    }
---
+++ src/main/kotlin/week06/week06Practice.kt
@@ -0,0 +1,10 @@
+package week06
+
+fun main() {
+    val x = 12
+    if(x in 15 .. 100){
+        println("yes")
+    } else{
+        println("false")
+    }
+}
